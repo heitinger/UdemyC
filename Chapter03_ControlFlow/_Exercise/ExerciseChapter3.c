c824a93d2b1eaef6c7d30b2ce4632f56578100de
Hannah Reitinger <hannah.reitinger@outlook.com>
2024-08-21 19:17:32 +0200

    exercise chap. 3

--- Chapter03_ControlFlow/_Exercise/ExerciseChapter3.c
+++ Chapter03_ControlFlow/_Exercise/ExerciseChapter3.c
@@ -2,6 +2,22 @@
 
 int main()
 {
+    int number;
+    printf("Please enter a valid unsigned integer!\n");
+    scanf("%d", &number);
+
+    int remainder = number % 3;
+
+    if (remainder == 0)
+    {
+        printf("The number you entered is div. by 3!\n");
+    }
+
+    else
+    {
+        printf("The number you entered is not div. by 3!\n");
+        printf("The remainder of the operation is: %d\n", remainder);
+    }
 
     return 0;
 }
