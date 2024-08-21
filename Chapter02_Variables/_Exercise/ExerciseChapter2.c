26383b94d0a72fe2b53da479473ddd1bc4b913da
Hannah Reitinger <hannah.reitinger@outlook.com>
2024-08-21 19:09:32 +0200

    exercise chap. 2

--- Chapter02_Variables/_Exercise/ExerciseChapter2.c
+++ Chapter02_Variables/_Exercise/ExerciseChapter2.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    unsigned int age;
+    printf("Please  enter your age in years: ");
+    scanf("%u", &age);
+
+    unsigned int days = age * 365;
+    unsigned int hours = days * 24;
+    unsigned int minutes = hours * 60;
+    unsigned int seconds = minutes * 60;
+
+    printf("You are living since %u days\n", days);
+    printf("You are living since %u hours\n", hours);
+    printf("You are living since %u minutes\n", minutes);
+    printf("You are living since %u seconds\n", seconds);
 
 
     return 0;
