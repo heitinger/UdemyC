38fbb1c04ef0b65b19dda2d46089eda733840926
Hannah Reitinger <hannah.reitinger@outlook.com>
2024-09-05 09:22:18 +0200

    chap04_exercise

--- Chapter04_Loops/_Exercise/ExerciseChapter4.c
+++ Chapter04_Loops/_Exercise/ExerciseChapter4.c
@@ -3,5 +3,30 @@
 int main()
 {
 
+int matrix_number;
+printf("Enter a number for your matrix: ");
+scanf("%d", &matrix_number);
+
+for (int k = 0; k <= matrix_number + 1; k++)
+{
+    printf("<  ");
+    for (int i = 1; i <= matrix_number; i++)
+    {
+        if (k == 0)
+        {
+            printf("  ^    ");
+        }
+        else if (k == matrix_number + 1)
+        {
+            printf("  v    ");
+        }
+        else
+        {
+            printf("(%d,%d)  ", k, i);
+        }
+    }
+    printf(">\n");
+}
+
     return 0;
 }
