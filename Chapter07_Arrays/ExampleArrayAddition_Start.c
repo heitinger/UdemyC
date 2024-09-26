313018d3254ae87f8ceeac30e693c7b003bbed09
Hannah Reitinger <hannah.reitinger@outlook.com>
2024-09-26 11:21:03 +0200

    example_arrays_1

--- Chapter07_Arrays/ExampleArrayAddition_Start.c
+++ Chapter07_Arrays/ExampleArrayAddition_Start.c
@@ -4,8 +4,25 @@
 int main()
 {
     float vector1[3] = {0.0f, 0.0f, 0.0f};
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter a number %d for vector 1: ", i);
+        scanf("%f", &vector1[i]);
+    }
+
     float vector2[3] = {0.0f, 0.0f, 0.0f};
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter a number %d for vector 2: ", i);
+        scanf("%f", &vector2[i]);
+    }
+
     float result[3] = {0.0f, 0.0f, 0.0f};
+    for (int i = 0; i < 3; i++)
+    {
+        result[i] = vector1[i] + vector2[i];
+        printf("%f\n", result[i]);
+    }
 
     return 0;
 }
