3a49f2ef9c0537fa41c2eedbd6c2e7d2b65c51f8
Hannah Reitinger <hannah.reitinger@outlook.com>
2024-09-26 10:32:59 +0200

    chap06_exercise

--- Chapter06_Pointers/_Exercise/Exercise/Main.c
+++ Chapter06_Pointers/_Exercise/Exercise/Main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int binom1 (int a, int b) //Call by Value
+{
+    int c = (a+b) * (a+b);
+    return c;
+}
+
+void binom2 (int a, int b, int* c) // Call by Reference
+{
+    *c = (a+b) * (a+b);
+}
+
 int main()
 {
     int a = 4;
@@ -7,8 +18,13 @@ int main()
     int c = 0;
 
     printf("a=%d, b=%d\n", a, b);
-    printf("Call binom function!\n");
-    binom(a, b, &c);
+
+    printf("Call binom function 1!\n");
+    c = binom1(a, b);
+    printf("c=%d\n", c);
+
+    printf("Call binom function 2!\n");
+    binom2(a, b, &c);
     printf("c=%d\n", c);
 
     return 0;
